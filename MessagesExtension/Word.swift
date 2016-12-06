aa9990227f092576b5c2df0cdf03bc0c4c885b0f
Drew Lanning <drewLanning@Drews-MacBook-Air-2.local>
2016-12-06 11:13:34 -0500

    got network requests and word fetching and definition fetching working, problem with creating new word while still waiting for definition to come back from API

--- MessagesExtension/Word.swift
+++ MessagesExtension/Word.swift
@@ -10,11 +10,5 @@ import Foundation
 
 struct Word {
   var name: String
-  var definition: [String: String]
-  
-  init(withJSON json: [String: AnyObject]) {
-    // parse json passed in and populate the name and definition of word?
-    // I feel like instead this word should make a network call via a network class
-    // and then parse the results
-  }
+  var definition: String
 }
--- MessagesExtension/WordsAPI.swift
+++ MessagesExtension/WordsAPI.swift
@@ -12,44 +12,54 @@ class WordsAPI {
   
   let baseURL = "https://wordsapiv1.p.mashape.com/words/"
   let header = ["X-Mashape-Key": "p8jiu8dllpmshOya3mO11qIH01dfp1ZitJLjsnHu4q9CflQbWY", "Accept": "application/json"]
+  //static let MAX_WORD_LENGTH = 6
+  let STARTER_WORDS = "starterWords"
   
-  func fetchWordData(forWord word: String) {
+  var word: String? = nil
+  var definition: String? = nil
+  
+  func fetchDefinition(forWord word: String) -> String? {
     let endpoint = baseURL + word
     let wordsURL = URL(string: endpoint)!
     var request = URLRequest(url: wordsURL)
-    print(wordsURL)
     request.allHTTPHeaderFields = header
     request.httpMethod = "GET"
-    let task = URLSession.shared.dataTask(with: request, completionHandler: {data , response, error in
-      guard error == nil else {
-        return
-      }
-      if let data = data, let response = response as? HTTPURLResponse {
-        if response.statusCode == 200 {
-          self.process(data: data)
-        } else {
-          // handle error here
-        }
-      }
-      
+    NetworkRequest.get(withRequest: request, completion: { data -> String in
+      self.process(data: data)
+      return self.definition!
     })
-    task.resume()
+    return "Still fetching definition..."
   }
   
   private func process(data: Data) {
-    if let json = try? JSONSerialization.jsonObject(with: data, options: []) as Any {
-      print(json)
+    if let json = try? JSONSerialization.jsonObject(with: data, options: []) as! [String:Any] {
+      let results = json["results"] as! [AnyObject]
+      for member in results {
+        let definition = member["definition"] as? String
+        self.definition = definition
+      }
     }
   }
   
-  // functions below should take the payload from the fetch,
-  // then parse it for whatever is being sought: 
-  // - definition
-  // - synonyms
-  // - part of speech, etc.
-  // The API may already offer this in a simple way, in which case great, these functions become wrappers
-  // for the API requests
+  func fetchRandomWord() -> String? {
+    // FETCH FROM LIST OF CURATED STARTER WORDS
+    if let path = Bundle.main.path(forResource: STARTER_WORDS, ofType: "txt") {
+      do {
+        let data = try String(contentsOfFile: path, encoding: .utf8)
+        let allWords = data.components(separatedBy: .newlines)
+        let random = Int(arc4random_uniform(UInt32(allWords.count)))
+        return allWords[random-1]
+      } catch {
+        print(error)
+        return nil
+      }
+    }
+    print("returning nil")
+    return nil
+  }
   
-  // What else would we need? Maybe add a bunch and if we don't use then we don't use?
+  func validate(word: String) -> Bool {
+    return false
+  }
   
 }
