cf984235f8263a52db2137445a07ce143a8f95b5
Drew Lanning <drewLanning@Drews-MacBook-Air-2.local>
2017-01-07 14:54:20 -0500

    made chainView subclass, committing before trying to fit into storyboard

---
+++ MessagesExtension/ChainView.swift
@@ -0,0 +1,32 @@
+//
+//  ChainView.swift
+//  Swapr
+//
+//  Created by Drew Lanning on 1/7/17.
+//  Copyright © 2017 Drew Lanning. All rights reserved.
+//
+
+import UIKit
+
+class ChainView: UIView {
+  
+  @IBOutlet weak var chainStack: UIStackView!
+  
+  func addChainsToStack(forScore score: Int) {
+    for chain in chainStack.subviews {
+      chain.removeFromSuperview()
+    }
+    let totalChainsToFit = (Double(superview!.bounds.width)/16).rounded()
+    for index in 0 ..< Int(totalChainsToFit) {
+      let chain: UIImageView
+      if score > index {
+        chain = UIImageView(image: #imageLiteral(resourceName: "Chain"))
+      } else {
+        chain = UIImageView(image: #imageLiteral(resourceName: "GrayChain"))
+      }
+      chain.tag = index
+      chainStack.addArrangedSubview(chain)
+    }
+  }
+
+}
